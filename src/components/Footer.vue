9fdaaa42ba06f1b418a2fe34db7cb7b9b1b4490b
Maria Dubyaga <kraftaa@gmail.com>
2019-02-06 19:52:41 -0500

    nesting components

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <p>{{ copyright }}</p>
+  </footer>
+
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      copyright: 'Copyright Vue'
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  footer{
+    background: #222;
+    padding: 6px;
+  }
+p{
+  color: lightgreen;
+  text-align: center;
+}
+</style>
+
+
