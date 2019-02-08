6398bcea6e995bc908dd81ce6cbbf0fc4b5e1ea0
Maria Dubyaga <kraftaa@gmail.com>
2019-02-08 18:47:01 -0500

    slot form

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,43 @@
 <template>
-  <div>
-    <form-helper>
-      <h2 slot="title">{{title}}</h2>
-      <p slot="text">I am the paragraph</p>
-    </form-helper>
-
-
-  </div>
+    <div>
+        <form-helper>
+            <div slot="form-header">
+                <h3>This is the title of a form</h3>
+                <p>This is some info about the form</p>
+            </div>
+            <div slot="form-fields">
+                <input type="text" placeholder="name" required />
+                <input type="password" placeholder="password" required />
+            </div>
+            <div slot="form-controls">
+                <button v-on:click="handleSubmit">Submit</button>
+            </div>
+        </form-helper>
+    </div>
 </template>
 
 <script>
-
+// Imports
 import formHelper from './components/formHelper.vue'
 export default {
     components: {
-      'form-helper': formHelper
+        'form-helper': formHelper
     },
     data () {
         return {
-          title:"dynamic slot title"
         }
     },
     methods: {
-
-
+        handleSubmit: function(){
+            alert('thanks for submitting');
+        }
     }
 }
 </script>
 
 <style>
-
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
 </style>
-
-
