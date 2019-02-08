2f793c4f5d002d5ea16eaa49c3d1c53a798c24c0
Maria Dubyaga <kraftaa@gmail.com>
2019-02-08 18:05:22 -0500

    slots

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,29 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+    <form-helper>
+      <h2 slot="title">{{title}}</h2>
+      <p slot="text">I am the paragraph</p>
+    </form-helper>
 
-    <app-footer v-bind:title="title"></app-footer>
+
   </div>
 </template>
 
 <script>
 
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Ninjas from './components/Ninjas.vue'
-
+import formHelper from './components/formHelper.vue'
 export default {
     components: {
-        'app-header': Header,
-        'app-footer': Footer,
-        'app-ninjas': Ninjas
+      'form-helper': formHelper
     },
     data () {
         return {
-          ninjas: [
-              {name: 'Ryu', speciality: 'Vue Components', show: false},
-              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-              {name: 'Hitoshi', speciality: 'Click Events', show: false},
-              {name: 'Tango', speciality: 'Conditionals', show: false},
-              {name: 'Kami', speciality: 'Webpack', show: false},
-              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-          ],
-          title: 'Vue Wizards'
+          title:"dynamic slot title"
         }
     },
     methods: {
-      updateTitle: function(updatedTitle){
-        this.title = updatedTitle;
-      }
+
+
     }
 }
 </script>
