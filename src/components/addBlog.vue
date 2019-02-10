781393b343fdaa339113b375c773e039e388c611
Maria Dubyaga <kraftaa@gmail.com>
2019-02-09 19:44:37 -0500

    select box binding

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -16,6 +16,10 @@
                 <label>Cheese</label>
                 <input type="checkbox" value="cheese" v-model="blog.categories"/>
             </div>
+            <label>Author:</label>
+            <select v-model="blog.author">
+                <option v-for="author in authors">{{ author }}</option>
+            </select>
         </form>
         <div id="preview">
             <h3>Preview blog</h3>
@@ -26,6 +30,7 @@
             <ul>
                 <li v-for="category in blog.categories">{{ category }}</li>
             </ul>
+            <p>Author: {{ blog.author }}</p>
         </div>
     </div>
 </template>
@@ -38,8 +43,10 @@ export default {
             blog: {
                 title: '',
                 content: '',
-                categories:[]
-            }
+                categories:[],
+                author:""
+            },
+            authors:['net ninja','avenger','vue vindicator']
         }
     },
     methods: {
