e2c0ee242e6564b3e9ebc55c92c9908ff6049541
Maria Dubyaga <kraftaa@gmail.com>
2019-02-14 22:19:33 -0500

    custom arguments

--- src/main.js
+++ src/main.js
@@ -12,6 +12,21 @@ Vue.directive('rainbow', {
     }
 })
 
+Vue.directive('theme', {
+    //functionality
+    // bind hook
+    bind(el, binding, vnode){
+        if(binding.value == "wide") {
+            el.style.maxWidth = "1200px";
+        } else if(binding.value == "narrow") {
+            el.style.maxWidth = "500px";
+        }
+        if(binding.arg == 'column') {
+            el.style.background = '#ddd';
+            el.style.padding = "20px";
+        }
+        }
+})
 Vue.use(VueResource)
 
 new Vue({
