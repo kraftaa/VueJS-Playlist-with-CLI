7119b18ea872dc9f28acd1830dcacc8f9e4eb451
Maria Dubyaga <kraftaa@gmail.com>
2019-02-14 22:01:23 -0500

    custom directives colors

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 
 import VueResource from 'vue-resource'
 
+// custom directives
+
+Vue.directive('rainbow', {
+    //functionality
+    bind(el, binding, vnode){
+        el.style.color = "#" + Math.random().toString().slice(2,8)
+    }
+})
+
 Vue.use(VueResource)
 
 new Vue({
